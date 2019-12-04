e9c2ef29f8d42d9298ca77698285de336ddfb0df
Corentin LEROY <corentin.leroy@epitech.eu>
2019-12-04 16:48:07 +0100

    Added Scenes to scene manager. Added Background to MainMenu. Added Panel behind menus. Added scene management within menus.
    Signed-off-by: Corentin LEROY <corentin.leroy@epitech.eu>

---
+++ Assets/Scripts/UI/CanvasManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CanvasManager : MonoBehaviour
+{
+    private GameObject canvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        canvas = GameObject.Find("Canvas");
+        SceneManager.sceneUnloaded += RedrawCanvas;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void RedrawCanvas<Scene> (Scene scene)
+    {
+        canvas.SetActive(true);
+    }
+}
