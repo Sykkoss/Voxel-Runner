2be2d0092b1ef0bb62534b23947f0a92bd0ef6ea
Corentin LEROY <corentin.leroy@epitech.eu>
2019-12-04 07:20:28 +0100

    Prototyped GameOverMenu, MainMenu, Settings, PauseMenu scene.
    Signed-off-by: Corentin LEROY <corentin.leroy@epitech.eu>

---
+++ Assets/Scripts/UI/ButtonManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonManager : MonoBehaviour
+{
+    public Sprite idleSprite;
+    public Sprite hoverSprite;
+    private bool isIdle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isIdle = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseOver()
+    {
+        transform.GetComponent<Image>().sprite = hoverSprite;
+    }
+
+    private void OnMouseExit()
+    {
+        transform.GetComponent<Image>().sprite = idleSprite;
+    }
+}
