b6f35a39fe2f3ffd09c5c9590e3ac76780d8f124
Xwilarg <xwilarg@yahoo.fr>
2019-12-16 12:06:11 +0900

    Add powerup generic class

---
+++ Assets/Scripts/PowerupProperties.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public static class PowerupProperties
+{
+    static PowerupProperties() // Init variables here
+    {
+        coinMultiplicatorPower = 2f;
+        coinMultiplicatorDuration = 4f;
+        magnetDuration = 4f;
+        invincibleDuration = 4f;
+    }
+
+    public static float coinMultiplicatorPower; // Multiplicator of the coin multiplicator powerup
+    public static float coinMultiplicatorDuration; // Duration in seconds
+
+    public static float magnetDuration; // Duration in seconds
+
+    public static float invincibleDuration; // Duration in seconds
+}
