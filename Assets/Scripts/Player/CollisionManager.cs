1eaee7e11ea36d9ac5fb43ff0deb71c40adf8f98
Kahlan <Kahlan@DESKTOP-GN8A9QR.local>
2019-12-17 17:54:41 +0900

    Pickup coin

---
+++ Assets/Scripts/Player/CollisionManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionManager : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider col)
+    {
+      if (col.gameObject.tag == "Coin") {
+        Destroy(col.gameObject);
+        PlayerPrefs.SetInt("Coins", PlayerPrefs.GetInt("Coins") + 1);
+      }
+    }
+}
---
+++ Assets/Scripts/UI/GameUI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameUI : MonoBehaviour
+{
+  public GameObject coinsNbr;
+    // Start is called before the first frame update
+    void Start()
+    {
+      coinsNbr.GetComponent<Text>().text = PlayerPrefs.GetInt("Coins").ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      coinsNbr.GetComponent<Text>().text = PlayerPrefs.GetInt("Coins").ToString();
+    }
+}
