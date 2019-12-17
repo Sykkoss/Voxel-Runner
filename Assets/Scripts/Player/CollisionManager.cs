193b99f515c72c7c0c51d21a56d3b389dc47a6a0
Xwilarg <xwilarg@yahoo.fr>
2019-12-17 23:40:35 +0900

    Add ability to take powerups

--- Assets/Scripts/Player/CollisionManager.cs
+++ Assets/Scripts/Player/CollisionManager.cs
@@ -1,14 +1,22 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CollisionManager : MonoBehaviour
 {
     private void OnTriggerEnter(Collider col)
     {
-      if (col.gameObject.tag == "Coin") {
-        Destroy(col.gameObject);
-        PlayerPrefs.SetInt("Coins", PlayerPrefs.GetInt("Coins") + 1);
-      }
+        if (col.gameObject.tag == "Coin")
+        {
+            Destroy(col.gameObject);
+            PlayerPrefs.SetInt("Coins", PlayerPrefs.GetInt("Coins") + 1);
+        }
+        else if (col.CompareTag("Powerup"))
+        {
+            switch (col.GetComponent<Powerup>().GetPowerupType())
+            {
+                default:
+                    throw new System.ArgumentException("Invalid powerup type");
+            }
+            Destroy(col.gameObject);
+        }
     }
 }
