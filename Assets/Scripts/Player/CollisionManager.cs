c98b2a638c6eda085ada1ee3f56cc5f9a7235246
Xwilarg <xwilarg@yahoo.fr>
2019-12-18 00:07:39 +0900

    Add coin multiplicator and invincibility

--- Assets/Scripts/Player/CollisionManager.cs
+++ Assets/Scripts/Player/CollisionManager.cs
@@ -1,22 +1,64 @@
 ﻿using UnityEngine;
 
+[RequireComponent(typeof(BoxCollider))]
 public class CollisionManager : MonoBehaviour
 {
+    private float timerMagnet, timerInvincible, timerMultiplicator;
+    private BoxCollider bc;
+
+    private void Start()
+    {
+        timerMagnet = 0f;
+        timerInvincible = 0f;
+        timerMultiplicator = 0f;
+        bc = GetComponent<BoxCollider>();
+    }
+
     private void OnTriggerEnter(Collider col)
     {
         if (col.gameObject.tag == "Coin")
         {
             Destroy(col.gameObject);
-            PlayerPrefs.SetInt("Coins", PlayerPrefs.GetInt("Coins") + 1);
+            PlayerPrefs.SetInt("Coins", PlayerPrefs.GetInt("Coins") + (1 * (timerMultiplicator > 0 ? PowerupProperties.coinMultiplicatorPower : 1)));
         }
         else if (col.CompareTag("Powerup"))
         {
             switch (col.GetComponent<Powerup>().GetPowerupType())
             {
+                case Powerup.PowerupType.Invincible:
+                    timerInvincible += PowerupProperties.invincibleDuration;
+                    break;
+
+                case Powerup.PowerupType.Magnet:
+                    timerMagnet += PowerupProperties.magnetDuration;
+                    break;
+
+                case Powerup.PowerupType.ScoreMultiplicator:
+                    timerMultiplicator += PowerupProperties.coinMultiplicatorDuration;
+                    break;
+
                 default:
                     throw new System.ArgumentException("Invalid powerup type");
             }
             Destroy(col.gameObject);
         }
+    }
+
+    private void Update()
+    {
+        timerMagnet -= Time.deltaTime;
+        timerInvincible -= Time.deltaTime;
+        timerMultiplicator -= Time.deltaTime;
+        if (timerMagnet <= 0f) timerMagnet = 0f;
+        if (timerInvincible <= 0f)
+        {
+            bc.enabled = true;
+            timerInvincible = 0f;
+        }
+        else
+        {
+            bc.enabled = false;
+        }
+        if (timerMultiplicator <= 0f) timerMultiplicator = 0f;
     }
 }
--- Assets/Scripts/PowerupProperties.cs
+++ Assets/Scripts/PowerupProperties.cs
@@ -4,13 +4,13 @@ public static class PowerupProperties
 {
     static PowerupProperties() // Init variables here
     {
-        coinMultiplicatorPower = 2f;
+        coinMultiplicatorPower = 2;
         coinMultiplicatorDuration = 4f;
         magnetDuration = 4f;
         invincibleDuration = 4f;
     }
 
-    public static float coinMultiplicatorPower; // Multiplicator of the coin multiplicator powerup
+    public static int coinMultiplicatorPower; // Multiplicator of the coin multiplicator powerup
     public static float coinMultiplicatorDuration; // Duration in seconds
 
     public static float magnetDuration; // Duration in seconds
