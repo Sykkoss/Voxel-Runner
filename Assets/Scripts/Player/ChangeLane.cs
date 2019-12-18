35c6f3b5e3a30d47e08bbd2d0f89852cb0c79c5c
Kahlan <Kahlan@DESKTOP-GN8A9QR.local>
2019-12-18 17:21:52 +0900

    smooth movement

--- Assets/Scripts/Player/ChangeLane.cs
+++ Assets/Scripts/Player/ChangeLane.cs
@@ -4,16 +4,14 @@ using UnityEngine;
 
 public class ChangeLane : MonoBehaviour
 {
-  private int lane = 0;
-  public enum Direction{
-  	Up,
-  	Left,
-  	Right,
-  	}
+  private bool isMoving;
+  private int lane = 1;
+  private Rigidbody controller;
     // Start is called before the first frame update
     void Start()
     {
-
+      isMoving = false;
+      controller = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -21,34 +19,44 @@ public class ChangeLane : MonoBehaviour
     {
       //Left
       if(Input.GetKeyDown(KeyCode.LeftArrow)){
-        Move(Direction.Left);
+        MoveLane(false);
       }
-
       //Right
       if(Input.GetKeyDown(KeyCode.RightArrow)){
-        Move(Direction.Right);
+        MoveLane(true);
+      }
+      if (isMoving == false) {
+        Vector3 targetPosition = transform.position.x * Vector3.forward;
+        targetPosition.y = transform.position.y;
+        targetPosition.z = transform.position.z;
+        if (lane == 0) {
+          targetPosition += Vector3.left * 0.9f;
+        } else if (lane == 2) {
+          targetPosition += Vector3.right * 0.9f;
+        }
+
+        Vector3 moveVector = Vector3.zero;
+        moveVector.x = (targetPosition - transform.position).normalized.x;
+        moveVector.y = 0f;
+        moveVector.z = 0f;
+        StartCoroutine(MovePlayer(targetPosition, moveVector * Time.deltaTime));
       }
     }
 
-    private void Move(Direction direction)
+    private IEnumerator MovePlayer(Vector3 targetPosition, Vector3 velocity) {
+      float duration = 2f;
+
+      isMoving = true;
+      while(Vector3.Distance(transform.position, targetPosition) > 0.01f) {
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, duration * Time.deltaTime);
+        yield return null;
+      }
+      transform.position = targetPosition;
+      isMoving = false;
+    }
+    private void MoveLane(bool goingRight)
     {
-      Vector3 newPosition = transform.position;
-
-      //Left
-      if(direction == Direction.Left){
-        if(lane != -1){
-          lane--;
-          newPosition.x -= 0.9f;
-        }
-      }
-
-      //Right
-      if(direction == Direction.Right){
-        if(lane != 1){
-          lane++;
-          newPosition.x += 0.9f;
-        }
-      }
-      transform.position =  newPosition;
+      lane += (goingRight) ? 1 : -1;
+      lane = Mathf.Clamp(lane, 0, 2);
     }
 }
