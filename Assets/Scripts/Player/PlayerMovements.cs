e58e5abe8701d6054aca2786aae19ff52cb2915a
Xwilarg <xwilarg@yahoo.fr>
2019-12-18 14:40:08 +0900

    Improve jump & slide

--- Assets/Scripts/Player/PlayerMovements.cs
+++ Assets/Scripts/Player/PlayerMovements.cs
@@ -6,40 +6,34 @@ public class PlayerMovements : MonoBehaviour {
     private BoxCollider coll;
     private bool isDoingAction;
 
-    private Vector3 baseCenter, baseSize;
-    private Vector3 smallSize;
-    private Vector3 jumpPos, slidePos;
+    private Vector3 baseSize;
 
 	private void Start ()
     {
         coll = GetComponent<BoxCollider>();
         isDoingAction = false;
-        baseCenter = coll.center;
         baseSize = coll.size;
-        smallSize = new Vector3(coll.size.x, .3f, coll.size.z);
-        jumpPos = new Vector3(coll.center.x, .2f, coll.center.z);
-        slidePos = new Vector3(coll.center.x, -.2f, coll.center.z);
     }
 
 	private void Update ()
     {
         if (Input.GetKeyDown(KeyCode.Space) && !isDoingAction)
         {
-            StartCoroutine(DoAction(jumpPos));
+            StartCoroutine(DoAction(new Vector3(0f, .9f, 0f), baseSize));
         }
         else if (Input.GetKeyDown(KeyCode.DownArrow) && !isDoingAction)
         {
-            StartCoroutine(DoAction(slidePos));
+            StartCoroutine(DoAction(Vector3.zero, new Vector3(baseSize.x, baseSize.y / 3f, baseSize.z)));
         }
     }
 
-    private IEnumerator DoAction(Vector3 center)
+    private IEnumerator DoAction(Vector3 center, Vector3 size)
     {
         isDoingAction = true;
         coll.center = center;
-        coll.size = smallSize;
+        coll.size = size;
         yield return new WaitForSeconds(.5f);
-        coll.center = baseCenter;
+        coll.center = Vector3.zero;
         coll.size = baseSize;
         isDoingAction = false;
     }
